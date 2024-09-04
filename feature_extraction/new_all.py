c6389270dd48066cb18f92f390c87ed16df79567
garlic0000 <garlic0000@163.com>
2024-09-04 20:27:21 +0800

    更新

--- feature_extraction/new_all.py
+++ feature_extraction/new_all.py
@@ -17,13 +17,11 @@ if __name__ == "__main__":
     # os.environ['CUDA_VISIBLE_DEVICES']    = '3, 4'
     # 只有0可以用
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-    # # 对cas(me)^2而言 不需要进行sampling
-    # print("================ sampling ================")
-    # # 顶点 采样？
-    # apex_sampling(opt)
+    print("================ sampling ================")
+    # 对数据集进行采样
+    apex_sampling(opt)
     print("================ crop ================")
     crop(opt)
-
     print("================ record ================")
     record_face_and_landmarks(opt)
     print("================ optical flow ================")
