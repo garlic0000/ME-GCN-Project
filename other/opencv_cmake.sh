0b782c85bd9b1ed670d01d75ee8926c27e5fb081
garlic0000 <garlic0000@163.com>
2024-09-05 20:15:36 +0800

    Update opencv_cmake.sh

--- other/opencv_cmake.sh
+++ other/opencv_cmake.sh
@@ -1,7 +1,6 @@
 cmake \
     -DBUILD_EXAMPLES=OFF \
     -DWITH_QT=OFF \
-    # -DCUDA_GENERATION=Auto \
     -DCUDA_GENERATION=Major6 \
     -DOpenGL_GL_PREFERENCE=GLVND \
     -DBUILD_opencv_hdf=OFF \
