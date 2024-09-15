09fa17897a3f8a7ba2ab25b4478ace967af1935a
garlic0000 <garlic0000@163.com>
2024-09-15 11:30:18 +0800

    Update opencv_cmake.sh

--- other/opencv_cmake.sh
+++ other/opencv_cmake.sh
@@ -46,7 +46,13 @@ cmake \
     -DOPENCV_ENABLE_PYTHON_GENERATION=OFF \
     -DBUILD_NEW_PYTHON_SUPPORT=OFF \
     -DHAVE_opencv_python3=OFF \
-    -DPYTHON_DEFAULT_EXECUTABLE="$(which python)" \
+    -DPYTHON_EXECUTABLE=/opt/conda/bin/python3 \
+    -DPYTHON_DEFAULT_EXECUTABLE=/opt/conda/bin/python3 \
+    -DPYTHON_EXECUTABLE=/opt/conda/bin/python3 \
+    -DPYTHON_INCLUDE_DIR=/opt/conda/include/python3.10 \
+    -DPYTHON_LIBRARY=/opt/conda/lib/libpython3.10.so \
+    -DPYTHON_NUMPY_INCLUDE_DIRS=/opt/conda/lib/python3.10/site-packages/numpy/core/include \
+    -DPYTHON_PACKAGES_PATH=/opt/conda/lib/python3.10/site-packages \
     -DGLOG_INCLUDE_DIR=/usr/include \
     -DGLOG_LIBRARY=/usr/lib/x86_64-linux-gnu/libglog.so \
     -DGFLAGS_INCLUDE_DIR=/usr/include \
