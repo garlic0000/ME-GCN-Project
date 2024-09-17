c6a11d0960b3d8b941124d4028ef95202794af5b
garlic0000 <garlic0000@163.com>
2024-09-17 17:31:09 +0800

    Update new_optflow.py

--- feature_extraction/cas(me)^2/new_optflow.py
+++ feature_extraction/cas(me)^2/new_optflow.py
@@ -49,6 +49,7 @@ def optflow(opt):
                         # 处理视频 获取光流特征
                         cmd = (f'denseflow "{str(type_item)}" -b=10 -a=tvl1 '
                                f'-s={opt_step} -if -o="{new_sub_dir_path}"')
+                        print(cmd)
                         os.system(cmd)
                         tq.update()
 
