3649a3f9300df5dd24837b2609c71584dd5b8a51
garlic0000 <garlic0000@163.com>
2024-09-28 09:39:25 +0800

    Update solve_sites.py

--- feature_extraction/cas(me)^2/solve_sites.py
+++ feature_extraction/cas(me)^2/solve_sites.py
@@ -1,7 +1,14 @@
 import numpy as np
 from collections import Counter
 import pandas as pd
+from pathlib import Path
+import os
+import glob
+import csv
+
 facebox_csv_root_path = "/kaggle/working/data/casme_2/faceboxcsv"
+
+
 def remove_outliers(data, m=1.5):
     """
     根据四分位数法则去掉极端值（异常值）
@@ -33,7 +40,25 @@ def get_most_frequent_average(data):
     # 计算平均值
     return np.mean(frequent_numbers)
 
-def merged_csv(csv_files_list):
+
+def example():
+    """
+    用于测试
+    """
+    # 示例数据
+    data = [10, 12, 12, 13, 13, 15, 15, 16, 16, 20, 30, 100, 150]
+
+    # 去掉极端值
+    filtered_data = remove_outliers(data)
+
+    # 计算剩余数据中频率最高的数的平均值
+    average_value = get_most_frequent_average(filtered_data)
+
+    print("去掉极端值后的数据:", filtered_data)
+    print("频率最高的数的平均值:", average_value)
+
+
+def merged_csv(csv_files_list, output_root_path):
     # 需要合并的 CSV 文件路径列表
     # csv_files = ['file1.csv', 'file2.csv']
 
@@ -42,21 +67,57 @@ def merged_csv(csv_files_list):
     merged_df = pd.concat(df_list, ignore_index=True)
 
     # 将合并后的结果写入一个新的 CSV 文件
-    merged_df.to_csv('merged_output.csv', index=False)
+    output_csv_path = os.path.join(output_root_path, 'merged_output.csv')
+    merged_df.to_csv(output_csv_path, index=False)
+    return output_csv_path
 
-    print(merged_df)
+
+def record_csv(csv_path, rows):
+    if os.path.exists(csv_path):
+        os.remove(csv_path)
+    with open(csv_path, 'w') as f:
+        csv_w = csv.writer(f)
+        csv_w.writerows(rows)
 
 
-# def get_sitelist():
+def get_facebox_average():
+    for sub_item in Path(facebox_csv_root_path).iterdir():
+        if not sub_item.is_dir():
+            continue
 
-# 示例数据
-data = [10, 12, 12, 13, 13, 15, 15, 16, 16, 20, 30, 100, 150]
+        for type_item in sub_item.iterdir():
+            if not type_item.is_dir():
+                continue
+            csv_path_list = glob.glob(os.path.join(str(type_item), "*.csv"))
+            output_root_path = os.path.join(facebox_csv_root_path, sub_item.name, type_item.name)
+            output_csv_path = None
+            if csv_path_list:
+                output_csv_path = merged_csv(csv_path_list, output_root_path)
+            if output_csv_path and os.path.exists(output_csv_path):
+                df = pd.read_csv(output_csv_path, header=None)
+                # 去掉极端值
+                filtered_data_left = remove_outliers(df[0])
+                # 计算剩余数据中频率最高的数的平均值
+                average_value_left = get_most_frequent_average(filtered_data_left)
+                # 去掉极端值
+                filtered_data_top = remove_outliers(df[1])
+                # 计算剩余数据中频率最高的数的平均值
+                average_value_top = get_most_frequent_average(filtered_data_top)
+                # 去掉极端值
+                filtered_data_right = remove_outliers(df[2])
+                # 计算剩余数据中频率最高的数的平均值
+                average_value_right = get_most_frequent_average(filtered_data_right)
+                # 去掉极端值
+                filtered_data_bottom = remove_outliers(df[3])
+                # 计算剩余数据中频率最高的数的平均值
+                average_value_bottom = get_most_frequent_average(filtered_data_bottom)
 
-# 去掉极端值
-filtered_data = remove_outliers(data)
+                facebox_average = [(average_value_left, average_value_top, average_value_right, average_value_bottom)]
 
-# 计算剩余数据中频率最高的数的平均值
-average_value = get_most_frequent_average(filtered_data)
+                print(facebox_average)
+                facebox_average_path = os.path.join(output_root_path, "facebox_average.csv")
+                record_csv(facebox_average_path, facebox_average)
 
-print("去掉极端值后的数据:", filtered_data)
-print("频率最高的数的平均值:", average_value)
+
+if __name__ == "__main__":
+    get_facebox_average()
