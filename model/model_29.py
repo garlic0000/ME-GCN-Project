83f7fbb57247928108245285aa9d4cc2a859c151
garlic0000 <garlic0000@163.com>
2025-01-11 13:26:03 +0800

    Update model_29.py

--- model/model_29.py
+++ model/model_29.py
@@ -239,6 +239,7 @@ class GCNWithMultiHeadGATAndTCN(nn.Module):
         self.bn2 = nn.BatchNorm1d(nout)
 
     def forward(self, x, adj):
+        x, reg_loss = self.graph_embedding(x, adj)
         print(f"Output from graph_embedding: {x}, {type(x)}, shape: {x.shape}")
         # 第一层 GCN
         x = self.gc1(x)
