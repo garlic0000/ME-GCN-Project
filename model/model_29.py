8a4da63b6d6a0d56354e531ccac11743f87a8619
garlic0000 <garlic0000@163.com>
2025-01-11 13:47:19 +0800

    Update model_29.py

--- model/model_29.py
+++ model/model_29.py
@@ -239,7 +239,6 @@ class GCNWithMultiHeadGATAndTCN(nn.Module):
         self.bn2 = nn.BatchNorm1d(nout)
 
     def forward(self, x, adj):
-        x, reg_loss = self.graph_embedding(x, adj)
         # 第一层 GCN
         x = self.gc1(x)
         x = self.bn1(x.transpose(1, 2)).transpose(1, 2)  # BatchNorm
@@ -250,7 +249,7 @@ class GCNWithMultiHeadGATAndTCN(nn.Module):
         x = self.tcn1(x.transpose(1, 2)).transpose(1, 2)
 
         # 没有第二层 TCN，直接返回
-        return x, reg_loss
+        return x
 
 
 class AUwGCNWithMultiHeadGATAndTCN(torch.nn.Module):
