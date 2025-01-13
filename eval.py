4c503d49dbc4445803a53bb3c976e5fafb5e43ea
garlic0000 <garlic0000@163.com>
2025-01-13 20:45:00 +0800

    更新

--- eval.py
+++ eval.py
@@ -1,6 +1,6 @@
 import torch
 import opts
-from model.model_36 import AUwGCNWithMultiHeadGATAndTCN
+from model.model_37 import AUwGCNWithMultiHeadGATAndTCN
 from datasets import LOSO_DATASET
 import os
 import yaml
