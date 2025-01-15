3b33b04932b3e3aa28e1f7355803cd334561a978
garlic0000 <garlic0000@163.com>
2025-01-15 15:53:29 +0800

    更新

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -1,6 +1,6 @@
 import torch
-from model.model_44 import GraphConvolution
-from model.model_44 import MultiHeadGraphAttentionLayer
+from model.model_45 import GraphConvolution
+from model.model_45 import MultiHeadGraphAttentionLayer
 
 
 # refer to https://github.com/karpathy/minGPT/blob/3ed14b2cec0dfdad3f4b2831f2b4a86d11aef150/mingpt/model.py#L136
