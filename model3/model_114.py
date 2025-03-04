267919daa4b9c2fadbd2eb0a10170c9fc9897a5a
garlic0000 <garlic0000@163.com>
2025-03-04 10:45:39 +0800

    Update model_114.py

--- model3/model_114.py
+++ model3/model_114.py
@@ -131,7 +131,7 @@ class AUwGCN(torch.nn.Module):
         super().__init__()
         mat_dir = '/kaggle/working/ME-GCN-Project'
         self.mat_path = os.path.join(mat_dir, 'assets', '{}.npy'.format(opt['dataset']))
-        self.graph_embedding = torch.nn.Sequential(GCN(2, 16, 16, self.mat_path, num_heads=4))
+        self.graph_embedding = GCN(2, 16, 16, self.mat_path, num_heads=4)
         # self.graph_embedding = torch.nn.Sequential(GCN(2, 32, 32, mat_path))
         in_dim = 192  # 24
 
