4776c222d954413288eb1053273871c1cea41eb1
garlic0000 <garlic0000@163.com>
2025-03-07 09:48:16 +0800

    更新

--- model3/model_121.py
+++ model3/model_121.py
@@ -7,14 +7,14 @@ import os
 import numpy as np
 
 """
-drop_prob=0.4
-min_drop_prob=0.05
+drop_prob=0.3
+min_drop_prob=0.01
 残差直接连接
 
 """
 
 
-def drop_edge(adj, drop_prob=0.4, epoch=0, max_epochs=100, min_drop_prob=0.05):
+def drop_edge(adj, drop_prob=0.3, epoch=0, max_epochs=100, min_drop_prob=0.01):
     """动态调整 DropEdge 概率
 
     参数:
@@ -47,7 +47,7 @@ class GraphConvolution(nn.Module):
     Simple GCN layer with Residual Connection and ResidualWeight Optimization
     """
 
-    def __init__(self, in_features, out_features, mat_path, bias=True, drop_prob=0.4, min_drop_prob=0.05):
+    def __init__(self, in_features, out_features, mat_path, bias=True, drop_prob=0.3, min_drop_prob=0.01):
         super(GraphConvolution, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
@@ -115,8 +115,8 @@ class MultiHeadGraphAttentionLayer(nn.Module):
     多头图注意力层 (Multi-Head GAT Layer)
     """
 
-    def __init__(self, in_features, out_features, num_heads=4, dropout=0.6, alpha=0.2, drop_prob=0.4,
-                 min_drop_prob=0.05):
+    def __init__(self, in_features, out_features, num_heads=4, dropout=0.6, alpha=0.2, drop_prob=0.3,
+                 min_drop_prob=0.01):
         super(MultiHeadGraphAttentionLayer, self).__init__()
 
         self.num_heads = num_heads
